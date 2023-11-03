f4605fb977a1c8af48e52dd94a622719662466cf
ahma0 <dsd932@naver.com>
2023-11-03 15:05:32 +0900

    프로젝트 생성

---
+++ src/main/java/ownpli/v2/ownplicollector/OwnpliCollectorApplication.java
@@ -0,0 +1,13 @@
+package ownpli.v2.ownplicollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnpliCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwnpliCollectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ownpli/v2/ownplicollector/OwnpliCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package ownpli.v2.ownplicollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnpliCollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
